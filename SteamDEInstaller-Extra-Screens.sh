444b70b0a410cf50036aad5c86bcfae7587273a8
WildTom1 <40327566+WildTom1@users.noreply.github.com>
2023-05-14 15:38:49 -0500

    Update SteamDEInstaller-Extra-Screens.sh
    Finishes the installer for multiscreen installer. IDK why but there may not be any audio

--- SteamDEInstaller-Extra-Screens.sh
+++ SteamDEInstaller-Extra-Screens.sh
@@ -10,20 +10,26 @@ wget https://github.com/WildTom1/Steam-BigPicture-DE/raw/main/SteamBigPicture_wi
 echo 'Xrandr to see avalible displays (y or n)?'
 read yn
 
-echo $yn
-
-if [$yn == 'y']
+if [[ $yn == y ]];#these were a pain to get running
     then
         xrandr
-elif [$yn == 'yes']
+elif [[ $yn == yes ]];
     then
         xrandr
 fi
 
 echo 'What is the identifer for your second screen, example "DVI-D"'
 
-read $screen
+read screen
 
-echo $screen
+sed -i "s|xrandr --output DVI-D-0 --off --auto|xrandr --output $screen --off --auto|g" SteamBigPicture_with_extra_screens.sh
 
-sed -i "s/'DIV-D-0'/$screen/g" SteamBigPicture_with_extra_screens.sh
+mv SteamBigPicture_with_extra_screens.sh SteamBigPicture.sh
+chmod +x SteamBigPicture.sh
+
+sudo mv SteamBigPicture.sh /usr/bin
+sudo mv Steam_BigPicture.desktop /usr/share/xsessions/
+
+cd ../
+rm -r SteamDEInstaller
+echo Steam BigPicture DE has been installed
