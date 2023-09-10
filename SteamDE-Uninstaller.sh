1e38c5573b95ab958556ec82a014e4070ef20fad
WildTom1 <40327566+WildTom1@users.noreply.github.com>
2023-09-09 21:21:09 -0500

    Update SteamDE-Uninstaller.sh

--- SteamDE-Uninstaller.sh
+++ SteamDE-Uninstaller.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#I am not going to use this anymore because last time I ran it it somehow broke my network that I needed to reboot to fix
 sudo rm /usr/share/xsessions/Steam_BigPicture.desktop 
 sudo rm /usr/bin/SteamBigPicture.sh 
 echo Steam BigPicture DE had been removed
